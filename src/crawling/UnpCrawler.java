71dfbe899a80585630e095dce529faba8780e250
Ali Sedaghat <a.sedaghat1372@gmail.com>
2016-01-10 17:52:10 +0330

    unpCrawler but it has some issues

--- src/crawling/UnpCrawler.java
+++ src/crawling/UnpCrawler.java
@@ -2,7 +2,16 @@ package crawling;
 
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.regex.Pattern;
 
 /**
  * Created by SAEED on 2016-01-07
@@ -15,13 +24,49 @@ import edu.uci.ics.crawler4j.url.WebURL;
  * url is: http://www.unp.ir/news/job
  */
 public class UnpCrawler extends WebCrawler {
+    Pattern filter = Pattern.compile
+            ("http://www\\.unp\\.ir/news/job((\\?page=[\\d]+)|(/.+/[\\d]+))");
+
+
     @Override
-    public boolean shouldVisit(Page referringPage, WebURL url) {
-        return super.shouldVisit(referringPage, url);
+    public boolean shouldVisit(Page page, WebURL url)  {
+        /*try {
+            String href = url.getURL().toLowerCase();
+            String decodedString = URLDecoder.decode(href, "UTF8");
+            return filter.matcher(decodedString).matches() &&
+                    !decodedString.startsWith("http://www.irankar.biz/category");
+        } catch (UnsupportedEncodingException ex) {
+            ex.printStackTrace();
+            return false;
+        }*/
+        String href = url.getURL();
+        return filter.matcher(href).matches();
     }
 
     @Override
     public void visit(Page page) {
-        super.visit(page);
+
+        if (page.getWebURL().getURL().equals("http://www.unp.ir/news/job"))
+            return;
+
+        else if (page.getWebURL().getURL().matches("http://www\\.unp\\.ir/news/job\\?page=[\\d]+")) {
+            return;
+        }
+
+        else {
+            try {
+                System.out.println(URLDecoder.decode(page.getWebURL().getURL(), "UTF8"));
+                Document doc = Jsoup.parse(((HtmlParseData) page.getParseData()).getHtml());
+                Elements elements = doc.select
+                        ("p font , .dirs:nth-child(2) , .newstitle a");
+                for (Element element : elements) {
+                    System.out.println(element.text());
+                    System.out.println("==========");
+                }
+            } catch (UnsupportedEncodingException ex) {
+                ex.printStackTrace();
+            }
+
+        }
     }
 }
