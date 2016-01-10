24add7ba9471fb6485c44006d97a3c09d6c11b99
Ali Sedaghat <a.sedaghat1372@gmail.com>
2016-01-10 15:51:27 +0330

    Ecity24 crawler

--- src/crawling/Ecity24Crawler.java
+++ src/crawling/Ecity24Crawler.java
@@ -2,7 +2,12 @@ package crawling;
 
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.regex.Pattern;
 
 /**
  * Created by SAEED on 2016-01-07
@@ -15,13 +20,51 @@ import edu.uci.ics.crawler4j.url.WebURL;
  * url is: http://www.ecity24.ir/RJobs.aspx
  */
 public class Ecity24Crawler extends WebCrawler {
+    Pattern filter = Pattern.compile
+            ("http://www\\.ecity24\\.ir/RJobDetails\\.aspx\\?JobID=.*");
+
+
     @Override
-    public boolean shouldVisit(Page referringPage, WebURL url) {
-        return super.shouldVisit(referringPage, url);
+    public boolean shouldVisit(Page page, WebURL url)  {
+        /*try {
+            String href = url.getURL().toLowerCase();
+            String decodedString = URLDecoder.decode(href, "UTF8");
+            return filter.matcher(decodedString).matches() &&
+                    !decodedString.startsWith("http://www.irankar.biz/category");
+        } catch (UnsupportedEncodingException ex) {
+            ex.printStackTrace();
+            return false;
+        }*/
+        String href = url.getURL();
+        return href.startsWith("http://www.ecity24.ir/RJobs.aspx");
     }
 
     @Override
     public void visit(Page page) {
-        super.visit(page);
+
+        if (page.getWebURL().getURL().equals("http://www.ecity24.ir/RJobs.aspx")) {
+            System.out.println("ali");
+
+            Document doc = Jsoup.parse(((HtmlParseData) page.getParseData()).getHtml());
+
+            System.out.println(doc.getElementsByTag("script").text());
+
+
+
+        /*else {
+            try {
+                System.out.println(URLDecoder.decode(page.getWebURL().getURL(), "UTF8"));
+                Document doc = Jsoup.parse(((HtmlParseData) page.getParseData()).getHtml());
+                Elements elements = doc.select
+                        ("#ctl00_ContentPlaceHolder1_Fieldset4 , #ctl00_ContentPlaceHolder1_lblPlaceJob , #ctl00_ContentPlaceHolder1_date , #ctl00_ContentPlaceHolder1_title");
+                for (Element element : elements) {
+                    System.out.println(element.text());
+                    System.out.println("==========");
+                }
+            } catch (UnsupportedEncodingException ex) {
+                ex.printStackTrace();
+            }*/
+
+        }
     }
 }
