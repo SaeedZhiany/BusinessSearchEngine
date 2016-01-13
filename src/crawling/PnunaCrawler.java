accd33a8607d9074b3f9e61eca8c6e141526d0ac
saeed-zh <gsilyp@gmail.com>
2016-01-14 01:03:23 +0330

    اصلاح پترن و نحوه ساخت تاریخ

--- src/crawling/PnunaCrawler.java
+++ src/crawling/PnunaCrawler.java
@@ -36,7 +36,8 @@ public class PnunaCrawler extends WebCrawler {
 
     private final ArrayList<Feed> feeds = new ArrayList<Feed>();
     Pattern filter = Pattern.compile("http://job\\.pnuna\\.com/page/[\\d]+/");
-    Pattern filter1 = Pattern.compile("http://job\\.pnuna\\.com/[\\d]+/.*استخدام.*");
+    Pattern filter1 = Pattern.compile("http://job\\.pnuna\\.com/[\\d]+/.*استخدام.*\\.html");
+
     @Override
     public boolean shouldVisit(Page referringPage, WebURL url) {
         try {
@@ -60,6 +61,7 @@ public class PnunaCrawler extends WebCrawler {
                 return;
             }
             else if (filter1.matcher(URLDecoder.decode(page.getWebURL().getURL(), "UTF8")).matches()) {
+                System.out.println(page.getWebURL().toString());
                 Document doc = Jsoup.parse(((HtmlParseData) page.getParseData()).getHtml());
                 Elements elements1 = doc.select(".post-info li:nth-child(2) , .box-top a");
                 Elements elements2 = doc.select(".post-info1 li:nth-child(1) a");
@@ -73,23 +75,29 @@ public class PnunaCrawler extends WebCrawler {
                 String city = elements2.text().trim();
 
                 //date
-                String[] date = elements1.get(elements1.size() - 1).text().split(DATE_SPLITTER)[1].split(DATE_DETAIL_SPLITTER);
-                date[1] = CalendarUtility.getNumericMonth(date[1]).toString(); // convert alphabet month to numeric
+                String[] dates = elements1.get(elements1.size() - 1).text().split(DATE_SPLITTER)[1].split(DATE_DETAIL_SPLITTER);
+                dates[1] = CalendarUtility.getNumericMonth(dates[1]); // convert alphabet month to numeric
+                String date = dates[2] + "/" +
+                        (dates[1].length() == 1?"0":"") +
+                        dates[1] + "/" +
+                        (dates[0].length() == 1?"0":"") +
+                        dates[0];
+                date = CalendarUtility.getEnglishDate(date);
+                System.out.println(date);
                 elements1.remove(elements1.get(elements1.size() - 1));
 
                 //body
                 String body = elements3.text().trim();
                 for (Element element : elements1)
                     body = body.concat(element.text().trim());
-for (int i=0;i<date.length;i++){
-    System.out.print(date[i]+"/");}
+
                 try {
                     Feed feed = new Feed(
                             title,
                             body,
                             city,
                             page.getWebURL().toString(),
-                            date[2] + "/" + date[1] + "/" + date[0],
+                            date,
                             pnunaDateFormat);
                     feeds.add(feed);
                 } catch (ParseException e) {
